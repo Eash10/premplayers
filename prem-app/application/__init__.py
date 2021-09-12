81760fece70cea210211f8a14beb0148aa3e708f
Ubuntu <ubuntu@ip-172-31-16-20.eu-west-1.compute.internal>
2021-09-12 23:42:50 +0000

    database

--- prem-app/application/__init__.py
+++ prem-app/application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from os import getenv
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eash:Eashdino10@appdatabase.cafuawxxfbj2.eu-west-1.rds.amazonaws.com:3306/database_project'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eash:Eashdino10@database.cafuawxxfbj2.eu-west-1.rds.amazonaws.com:3306/database_project'
 app.config['SECRET_KEY'] = "MY_SECRET_KEY"
 
 

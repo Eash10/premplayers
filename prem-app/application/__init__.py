81747a9635d7d6d0843542b4a2ffbc56d181dc17
Ubuntu <ubuntu@ip-172-31-16-20.eu-west-1.compute.internal>
2021-09-13 06:02:32 +0000

    hiding database and key

--- prem-app/application/__init__.py
+++ prem-app/application/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from os import getenv
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eash:Eashdino10@appdatabase.cafuawxxfbj2.eu-west-1.rds.amazonaws.com:3306/database_project'
-app.config['SECRET_KEY'] = "MY_SECRET_KEY"
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = getenv("MY_SECRET_KEY")
 
 
 db = SQLAlchemy(app)
